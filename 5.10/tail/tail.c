cf9a8d8636b63ece6117093875e52407046ba3a4
William English <ekg98@sublimeparadise.com>
2018-08-23 02:06:34 -0400

    Working tail.c... Partially functional.

--- 5.10/tail/tail.c
+++ 5.10/tail/tail.c
@@ -5,14 +5,19 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 #include "alloc.h"	/* header file for alloc function char *alloc(int) */
 
+#define	INPUTBUF	1024	/* Maximum buffer size */
+
 int getaline(char *s, int limit);
+int push(char *string, char *linesarray[], int linelimit);
 
 int main(int argc, char *argv[])
 {
 	int argumentCounter = 1;
 	int lineQuantity = 10;	/* default line quantity */
+	int i;
 
 	if(argc > 1)	/* sets lineQuantity if a argument is present */
 	{
@@ -25,7 +30,7 @@ int main(int argc, char *argv[])
 				else
 				{
 					printf("Argument error.\n");
-					return -1;
+					return 1;
 				}
 			}
 			argumentCounter++;
@@ -34,15 +39,28 @@ int main(int argc, char *argv[])
 	}
 
 	char *lines[lineQuantity];	/* Allocate pointer array for lines based off default value or arguments */
+	char inputLineBuffer[INPUTBUF];
 
+	for(i = 0; i < lineQuantity - 1; i++)	/* NULL the pointer array */
+		lines[i] = NULL;	
 
+	while(getaline(inputLineBuffer, INPUTBUF) > 0)
+	{
+		if(push(inputLineBuffer, lines, lineQuantity))
+		{
+			printf("Error: Could not allocate enough memory.\n");
+			return 1;
+		}
+	}
 
-	printf("argument value = %d\n", lineQuantity);
-	
+	for(i = 0; i < lineQuantity - 1; i++)
+		/*if(lines[i] != NULL)*/
+			printf("%s",lines[i]);
 	
 	return 0;
 }
 
+/* Standard getline function.  Grabs keyboard input and populates the string to the limit. */
 int getaline(char *s, int limit)
 {
 	int c, i;
@@ -60,3 +78,30 @@ int getaline(char *s, int limit)
 
 	return i;
 }
+
+int push(char *string, char *linesarray[], int linelimit)
+{
+	int stringsize;
+	char *newstring = NULL;
+	char *tempstring = NULL;
+	int i;
+
+	stringsize = (strlen(string) + 1);
+
+	if((newstring = alloc(stringsize)) == NULL)	/* Allocate memory for the string transfer */
+		return 1;
+
+	strcpy(newstring, string);			/* copy the new string */
+	
+	tempstring = *linesarray;
+	*linesarray = newstring;
+
+	for(i = 1; i < linelimit - 1; i++)
+	{
+		linesarray[i] = tempstring;		/* put the tempstring into the current linearray location */
+	       	tempstring = *((linesarray + i) + 1);	/* get the next string pointer for the next loop run */
+	}
+
+	return 0;
+}
+
