074719a22159195a66d7b62252156a6698a341c0
William English <ekg98@sublimeparadise.com>
2018-08-29 04:02:31 -0400

    Finished ex5-15...

--- 5.11/ex5-15/ex5-15.c
+++ 5.11/ex5-15/ex5-15.c
@@ -21,8 +21,10 @@ void writelines(char *lineptr[], int nlines);
 void qsortnew(void *lineptr[], int left, int right, int (*comp)(void *, void *));
 void qsortignorecase(void *lineptr[], int left, int right, int (*comp)(void *, void *));
 int numcmp(char *, char *);
+int strcmpfold(char *, char *);
 int rnumcmp(char *, char *);
 int rstrcmp(char *, char *);
+int rstrcmpfold(char *, char*);
 
 int main(int argc, char *argv[])
 {
@@ -33,7 +35,6 @@ int main(int argc, char *argv[])
 	int i;
 	int argcounter = 1;
 	void (*compare) = NULL;
-	void (*sortfunction)(void **, int, int , int (*)(void *, void *));	/* Function pointer for qsorting function declared to hold pointer array, two ints, and a function pointer to two voids and return a int */
 
 	if(argc > 1)
 	{
@@ -53,17 +54,26 @@ int main(int argc, char *argv[])
 
 			if(*argv[i] == '-')					/* detect the '-' marker */
 			{
+
+				if(strlen(argv[i]) == 1)			/* check argument length and if it has nothing follow the '-' generate error message and quit. */
+				{
+					printf("Error: Unknown argument!\n");
+					return 1;
+				}
+
 				while(*(argv[i] + argcounter))			/* walk through the string and search for valid arguments */
 				{
 					if(*(argv[i] + argcounter) == 'n')	/* n for numeric ordering */
 						numeric = TRUE;					
-
-					if(*(argv[i] + argcounter) == 'r')	/* r for reverse search */
+					else if(*(argv[i] + argcounter) == 'r')	/* r for reverse search */
 						reverse = TRUE;
-					
-					if(*(argv[i] + argcounter) == 'f')	/* f for folding case.  (ignore case) */
+					else if(*(argv[i] + argcounter) == 'f')	/* f for folding case.  (ignore case) */
 						fold = TRUE;
-
+					else
+					{
+						printf("Error: Unknown argument!\n");
+						return 1;
+					}
 					argcounter++;
 				}
 			}
@@ -72,26 +82,33 @@ int main(int argc, char *argv[])
 
 	}
 
-	if(numeric == FALSE && reverse == FALSE)	/* default activated if no flags are present */
+	if(numeric == FALSE && reverse == FALSE && fold == FALSE)		/* default activated if no flags are present */
 		compare = strcmp;
 
-	if(numeric == FALSE && reverse == TRUE)		/* Activate reverse strcmp if flag solo r was detected */
+	if(numeric == FALSE && reverse == FALSE && fold == TRUE)		/* Activate strcmpfold if f flag is solo. (ignore case) */
+		compare = strcmpfold;
+
+	if(numeric == FALSE && reverse == TRUE && fold == FALSE)		/* Activate reverse strcmp if flag solo r was detected */
 		compare = rstrcmp;
 
-	if(numeric == TRUE && reverse == FALSE)		/* Activate numaric compare if flag solo n was detected*/
+	if(numeric == FALSE && reverse == TRUE && fold == TRUE)			/* Activate reverse strcmpfold if flag rf was detected */
+		compare = rstrcmpfold;
+
+	if(numeric == TRUE && reverse == FALSE && fold == FALSE)		/* Activate numaric compare if flag solo n was detected*/
 		compare = numcmp;
 
-	if(numeric == TRUE && reverse == TRUE)		/* Activate reverse numeric compare if the flags were a combination of r & n */
+	if(numeric == TRUE && reverse == TRUE && fold == FALSE)			/* Activate reverse numeric compare if the flags were a combination of r & n */
 		compare = rnumcmp;
 
-	if(fold == TRUE)				/* Activate folding.  (Ignore case during sorting operations).  Changes the sorting function depending on the f flag. */
-		sortfunction = qsortignorecase;
-	else
-		sortfunction = qsortnew;
+	if(numeric == TRUE && fold == TRUE)					/* Generate error if fold is used with number compare */
+	{
+		printf("Error: Fold option only works on strings.");
+		return 1;
+	}
 
 	if((nlines = readlines(lineptr, MAXLINES)) >= 0)
 	{
-		sortfunction((void **) lineptr, 0, nlines - 1, (int (*)(void *, void *))(compare));	/* Function pointer.  Can change depending on f flag */
+		qsortnew((void **) lineptr, 0, nlines - 1, (int (*)(void *, void *))(compare));	/* Function pointer.  Can change depending on f flag */
 		writelines(lineptr, nlines);
 		return 0;
 	}
@@ -120,26 +137,6 @@ void qsortnew(void *v[], int left, int right, int (*comp)(void *, void *))
 	swap(v, left, last);
 	qsortnew(v, left, last - 1, comp);
 	qsortnew(v, last + 1, right, comp);
-}
-
-/* Qsort for folding case.  (ignore case) */
-void qsortignorecase(void *v[], int left, int right, int (*comp)(void *, void *))
-{
-	int i, last;
-	void swap(void *v[], int, int);
-
-	if(left >= right)
-		return;
-
-	swap(v, left, (left + right) / 2);
-	last = left;
-
-	for(i = left + 1; i <= right; i++)
-		if((*comp)(v[i], v[left]) < 0)
-			swap(v, ++last, i);
-	swap(v, left, last);
-	qsortignorecase(v, left, last - 1, comp);
-	qsortignorecase(v, last + 1, right, comp);
 }
 
 /* standard numcmp */
@@ -174,6 +171,15 @@ int rnumcmp(char *s2, char *s1)
 		return 0;
 }
 
+/* strcmpfold */
+int strcmpfold(char *s, char *t)
+{
+	for( ; tolower(*s) == tolower(*t); s++, t++)
+		if(*s == '\0')
+			return 0;
+	return tolower(*s) - tolower(*t);
+}
+
 /* Reverse strcmp */
 int rstrcmp(char *t, char *s)
 {
@@ -181,6 +187,15 @@ int rstrcmp(char *t, char *s)
 		if(*s == '\0')
 			return 0;
 	return *s - *t;
+}
+
+/* Reverse strcmpfold */
+int rstrcmpfold(char *t, char *s)
+{
+	for( ; tolower(*s) == tolower(*t); s++, t++)
+		if(*s == '\0')
+			return 0;
+	return tolower(*s) - tolower(*t);
 }
 
 /* swap function for qsort */
