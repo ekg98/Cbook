0bf90bd4dfa3c7f56eda317e892ce7c9e1cb067f
William English <ekg98@sublimeparadise.com>
2018-10-09 13:50:53 -0400

    Made chapter 6.3 program work...  Now on to working the exercise ex6-1...

--- 6.3/prog1/prog1.c
+++ 6.3/prog1/prog1.c
@@ -8,14 +8,16 @@
 #define MAXWORD	100
 #define NKEYS (sizeof keytab / sizeof(struct key))
 
-int getword(char *, int);
-int binsearch(char *, struct key *, int);
-
 struct key
 {
 	char *word;
 	int count;
-} keytab[] = {
+};
+
+int getword(char *, int);
+int binsearch(char *, struct key *, int);
+
+struct key keytab[] = {
 	"auto",		0,
 	"break",	0,
 	"case",		0,
@@ -35,13 +37,17 @@ int main()
 	char word[MAXWORD];
 
 	while(getword(word, MAXWORD) != EOF)
-		if(!isalpha(word[0]))
+	{
+		if(isalpha(word[0]))
 			if((n = binsearch(word, keytab, NKEYS)) >= 0)
 				keytab[n].count++;
+	}
 
 	for(n = 0; n < NKEYS; n++)
+	{
 		if(keytab[n].count > 0)
 			printf("%4d %s\n", keytab[n].count, keytab[n].word);
+	}
 
 	return 0;
 }
