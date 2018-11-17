b3b05f3f8da157613bb4c6ca7c5722aeffa53a8d
William English <ekg98@indy.rr.com>
2018-11-17 14:17:56 -0500

    Working... 6.5 prog1...

---
+++ 6.5/prog1/prog1.c
@@ -0,0 +1,36 @@
+/* Program from the chapter 6.5 */
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define	MAXWORD	100
+
+void treeprint(struct tnode *);
+
+int main()
+{
+	struct tnode *root;
+	char word[MAXWORD];
+
+	root = NULL;
+
+	while(getword(word, MAXWORD) != EOF)
+		if(isalpha(word[0]))
+			root = addtree(root, word);
+
+	treeprint(root);
+
+	return 0;
+}
+
+/* treeprint: in-order print of tree p */
+void treeprint(struct tnode *p)
+{
+	if(p != NULL)
+	{
+		treeprint(p->left);
+		printf("%4d %s\n", p->count, p->word);
+		treeprint(p->right);
+	}
+}
