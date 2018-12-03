37ddd4c520ad1db9ab98321be2206d06f8419e65
William English <ekg98@sublimeparadise.com>
2018-12-02 21:21:34 -0500

    Working on ex6-2...  Almost working as expected...

--- 6.5/ex6-2/ex6-2.c
+++ 6.5/ex6-2/ex6-2.c
@@ -15,10 +15,13 @@
 #define	MAXWORD	100
 
 /* function declarations */
-char *treeprint(struct tnode *, int, struct tnode *, int);
+void treeprint(struct tnode *, int);
 
 /* enums */
-enum {ROOT, LEFTMATCH, NOTHING};
+enum {ROOT, LEFTMATCH, RIGHTMATCH, NOTHING};
+
+/* global variables */
+struct tnode *previous = NULL;
 
 int main(int argc, char *argv[])
 {
@@ -73,42 +76,22 @@ int main(int argc, char *argv[])
 		if(isalpha(word[0]))
 			root = addtree(root, word);
 
-	treeprint(root, argWordLength, NULL, ROOT);
+	treeprint(root, argWordLength);
 
 	return 0;
 }
 
 /* treeprint: in-order print of tree p */
-char *treeprint(struct tnode *p, int length, struct tnode *oldparentStruct, int operationFlag)
+void treeprint(struct tnode *p, int length)
 {
-	char *returnedWord = NULL;
-	int sendFlag = NOTHING;
 	if(p != NULL)
 	{
-		/* checking left child.  Returning its word */
-		returnedWord = treeprint(p->left, length, p, sendFlag);
 
-		/* check left child to parent.  */
-		if(strlen(p->word) >= length && returnedWord != NULL && strncmp(p->word, returnedWord, length) == 0)
-		{
-			printf("%4d %s\n", p->left->count, p->left->word);
+		treeprint(p->left, length);
+		if(previous != NULL && strncmp(p->word, previous->word, length) == 0)
 			printf("%4d %s\n", p->count, p->word);
-			sendFlag = LEFTMATCH;
-		}
+		previous = p;
+		treeprint(p->right, length);
 
-		/* check right child to parent */
-		else if(strlen(p->word) >= length && oldparentStruct != NULL && strncmp(p->word, oldparentStruct->word, length) == 0)
-		{
-			if(sendFlag != LEFTMATCH)
-				printf("%4d %s\n", oldparentStruct->count, oldparentStruct->word);	/* this results in duplicates if parent was already printed in previous check on the left to parent */
-
-			printf("%4d %s\n", p->count, p->word);
-		}
-
-		/* checking right child.  Sending its parent */
-		treeprint(p->right, length, p, sendFlag);
-
-		return p->word;
 	}
-	return NULL;
 }
