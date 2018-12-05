72a83a2344da7d1efd9bbb1981d9e87cdd01f6dd
William English <ekg98@sublimeparadise.com>
2018-12-04 23:05:52 -0500

    Generated framework for ex6-3 from main program of chapter 6.5...

---
+++ 6.5/ex6-3/ex6-3.c
@@ -0,0 +1,40 @@
+/* Exercise 6-3.  Write a cross-referencer that prints a list of all words in a document, and for each word, a list of the line numbers on which it occurs. */
+/* Remove noise words like "the", "and", and so on. */
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "addtree.h"
+#include "tnode.h"
+#include "getaword.h"
+
+#define	MAXWORD	100
+
+void treeprint(struct tnode *);
+
+int main()
+{
+	struct tnode *root;
+	char word[MAXWORD];
+
+	root = NULL;
+
+	while(getaword(word, MAXWORD) != EOF)
+		if(isalpha(word[0]))
+			root = addtree(root, word);
+
+	treeprint(root);
+
+	return 0;
+}
+
+/* treeprint: in-order print of tree p */
+void treeprint(struct tnode *p)
+{
+	if(p != NULL)
+	{
+		treeprint(p->left);
+		printf("%4d %s\n", p->count, p->word);
+		treeprint(p->right);
+	}
+}
---
+++ 6.5/ex6-3/getaword.h
@@ -0,0 +1,3 @@
+/* getaword.h:  Header file for getaword */
+
+int getaword(char *, int);
